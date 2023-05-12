322e12004ea178cfec0bd596ca7134785c55df24
Shivansh Khunger <khunger.shivansh@outlook.com>
2023-05-13 02:40:00 +0530

    Changed Some Minor Things

--- Owner-Side/Login-Page/login2.js
+++ Owner-Side/Login-Page/login2.js
@@ -26,6 +26,11 @@ function is_crct(username, password) {
       // The user has been logged in successfully
       return 1;
     }
+    if (user.username === username || user.password == password) {
+      return 2;
+      // The user has entered one correct and one incorrect value
+      // therefore we are saying to them that they should try again
+    }
   }
   return 0;
 }
@@ -45,9 +50,13 @@ document.getElementById("login-button").addEventListener("click", () => {
   var is_correct = is_crct(username, password);
   const home = document.getElementById("Home-Button");
   const billing = document.getElementById("Billing-Button");
-  if (is_correct) {
+  if (is_correct === 1) {
     billing.click();
     return;
+  } else if (is_correct === 2) {
+    alert("Invalid username or password. Try Again!");
+  } else {
+    home.click();
+    alert("Invalid username and password. Returning to Main Page!");
   }
-  alert("Invalid username or password.");
 });
